663f2215da89e17706a22aac22543452670fb6e2
Juan Lucas Rossi <juan.lucas.rossi@gmail.com>
2023-05-10 19:59:46 -0300

    adiciona camada de persistencia e adiciona helpers de inicialização de banco e de conexão com banco

---
+++ src/main/java/com/example/marisa/persistence/utils/DatabaseBuilder.java
@@ -0,0 +1,47 @@
+package com.example.marisa.persistence.utils;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseBuilder {
+
+    public void buildDatabaseIfMissing(){
+        if(!isDatabaseAvailable()){
+            System.out.println("Database is missing. Building database: \n");
+            buildTables();
+        }
+    }
+
+    private boolean isDatabaseAvailable(){
+        return Files.exists(Paths.get("database.db"));
+    }
+
+    private void buildTables() {
+        try (Statement stmt = DatabaseConnectionFactory.createStatement()) {
+            stmt.addBatch(createAddressTableSql());
+            stmt.executeBatch();
+
+            System.out.println("Database successfully created.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private String createAddressTableSql(){
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("CREATE TABLE Address (\n");
+        builder.append("id INTEGER PRIMARY KEY, \n");
+        builder.append("street TEXT NOT NULL, \n");
+        builder.append("number INTEGER, \n");
+        builder.append("city TEXT NOT NULL, \n");
+        builder.append("state TEXT NOT NULL, \n");
+        builder.append("zipCode TEXT, ");
+        builder.append("); \n");
+
+        System.out.println(builder.toString());
+        return builder.toString();
+    }
+}
