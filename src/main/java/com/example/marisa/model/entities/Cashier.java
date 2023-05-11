d0b894b903ec202746a342003c5e01e589fb730f
Eduardo Correa <eduardocorreass25@gmail.com>
2023-05-10 21:39:21 -0300

    feat: adiciona getters, setters e constructor

--- src/main/java/com/example/marisa/model/entities/Cashier.java
+++ src/main/java/com/example/marisa/model/entities/Cashier.java
@@ -8,9 +8,47 @@ public class Cashier {
     private float finalBalance;
     private String status;
 
+    public Cashier(int id, float openingBalance, float finalBalance, String status) {
+        this.setId(id);
+        this.setOpeningBalance(openingBalance);
+        this.setFinalBalance(finalBalance);
+        this.setStatus(status);
+    }
     public void openCashier(){}
 
     public void closeChasier(){}
 
     public void calculateDaySales() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getOpeningBalance() {
+        return openingBalance;
+    }
+
+    public void setOpeningBalance(float openingBalance) {
+        this.openingBalance = openingBalance;
+    }
+
+    public float getFinalBalance() {
+        return finalBalance;
+    }
+
+    public void setFinalBalance(float finalBalance) {
+        this.finalBalance = finalBalance;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
